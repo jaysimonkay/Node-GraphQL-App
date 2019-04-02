fa980b7fdd8ca694e566ff25763f9798b3f6118a
Jude Kuti <judekuti@gmail.com>
2019-04-02 13:06:55 -0500

    Create instance of mongoose andplug into mongod

--- server/app.js
+++ server/app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/node-graphql-app', { promiseLibrary: require('bluebird'), useNewUrlParser: true })
+    .then(() => console.log('connection successful'))
+    .catch((err) => console.error(err));
 
 var app = express();
 
@@ -24,18 +29,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
