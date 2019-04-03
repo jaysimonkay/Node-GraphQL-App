3b11917b4c573e9e5501641858a0e40777f7e602
Jude Kuti <judekuti@gmail.com>
2019-04-02 23:20:54 -0500

    Create books components

--- server/client/src/app/app.module.ts
+++ server/client/src/app/app.module.ts
@@ -7,9 +7,13 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ApolloModule, Apollo } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { BooksComponent } from './books/books.component';
+import { DetailsComponent } from './books/details/details.component';
+import { AddComponent } from './books/add/add.component';
+import { EditComponent } from './books/edit/edit.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BooksComponent, DetailsComponent, AddComponent, EditComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
