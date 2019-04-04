fa1172e3b594c88f1e2dd108897e0e01b3405f78
Jude Kuti <judekuti@gmail.com>
2019-04-03 22:54:46 -0500

    Relocate client to same level with server

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { DetailsComponent } from './books/details/details.component';
+import { AddComponent } from './books/add/add.component';
+import { EditComponent } from './books/edit/edit.component';
+
+const routes: Routes = [
+  {
+    path: 'books',
+    component: BooksComponent,
+    data: { title: 'List of Books' }
+  },
+  {
+    path: 'books/details/:id',
+    component: DetailsComponent,
+    data: { title: 'Book Details' }
+  },
+  {
+    path: 'books/add',
+    component: AddComponent,
+    data: { title: 'Add Book' }
+  },
+  {
+    path: 'books/edit/:id',
+    component: EditComponent,
+    data: { title: 'Edit Book' }
+  },
+  { path: '', redirectTo: '/books', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ client/src/app/books/books.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Book } from './book';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.scss']
+})
+export class BooksComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
