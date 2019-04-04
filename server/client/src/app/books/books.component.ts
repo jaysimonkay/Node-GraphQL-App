0599ded4f6eb1924c3a60ddda86420b0a0b66dfa
Jude Kuti <judekuti@gmail.com>
2019-04-03 23:10:36 -0500

    Include graphql and apollo for the books component

--- server/client/src/app/books/books.component.ts
+++ server/client/src/app/books/books.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Book } from './book';
 
 @Component({
   selector: 'app-books',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.scss']
 })
 export class BooksComponent implements OnInit {
+  displayedColumns: string[] = ['title', 'author'];
+  data: Book[] = [];
+  resp: any = {};
+  isLoadingResults = true;
 
-  constructor() { }
+  constructor(private apollo: Apollo) {}
 
   ngOnInit() {
+    this.apollo
+      .query({
+        query: gql`
+          {
+            books {
+              _id
+              title
+              author
+            }
+          }
+        `
+      })
+      .subscribe(res => {
+        this.resp = res;
+        this.data = this.resp.data.books;
+        console.log(this.data);
+        this.isLoadingResults = false;
+      });
   }
-
 }
